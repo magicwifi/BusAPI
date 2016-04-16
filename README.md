fd68662ef10284747aa346504568831b71f5ed37
magicwifi <huangzhe1218@163.com>
2016-04-16 16:43:25 +0800

    first

---
+++ README.md
@@ -0,0 +1,36 @@
+# 对外接口（写操作） 
+ 
+### 创建预约: Post /reservations/tabke
+
+参数说明 
+
+* passenger_id: 乘客号（非空）  
+* bus_number_id: 车次号（非空）
+
+
+测试程序 python create_reservation.py  5  3
+五号乘客预约3号车次
+
+返回值：
+
+* {result：200} 调用成功
+* {result：400} 调用失败
+
+
+
+### 签到: Post /takes/doctors
+
+参数说明 
+
+* passenger_id: 乘客号（非空）  
+* bus_number_id: 车次号（非空）
+
+
+测试程序 python create_takes.py  5  3
+五号乘客登上3号车次
+
+返回值：
+
+* {result：200} 调用成功
+* {result：400} 调用失败
+
---
+++ create_reservation.py
@@ -0,0 +1,25 @@
+#-*- coding:utf-8 -*-
+import requests
+import sys
+from sys import argv
+from bs4 import BeautifulSoup
+import urllib
+import urllib2
+import json
+
+def main(passenger_id,bus_number_id):
+        data = {'passenger_id':passenger_id, 'bus_number_id':bus_number_id}
+        url = "http://219.141.189.132:3000/reservations/register"
+        req = urllib2.Request(url)
+        req.add_header('Content-Type', 'application/json')
+        response = urllib2.urlopen(req,json.dumps(data))
+        result = response.read()
+        print result
+
+if __name__ == '__main__':
+        if len(sys.argv) != 3:
+                print 'Usage: python input_name output_name'
+                exit(1)
+        passenger_id = sys.argv[1]
+        bus_number_id = sys.argv[2]
+        main(passenger_id,bus_number_id)
